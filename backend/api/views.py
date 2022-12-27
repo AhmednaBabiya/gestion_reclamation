c0731cb62743b29073c4d4b0e9486953047ce631
AhmednaBabiya <babiyaahmedna@gmail.com>
2022-12-27 11:59:49 +0000

    attempt 1

--- backend/api/views.py
+++ backend/api/views.py
@@ -57,8 +57,10 @@ class ReclamationUpdateDetails(generics.RetrieveUpdateDestroyAPIView):
         serializer = ReclamationSerializer(
             reclamation, data=request.data, partial=True, context={'request': request})
         if request.data['status'] == 'Traitée':
+            _mutable = request.data._mutable
             request.data._mutable = True
             request.data['updated_by'] = f'{user.first_name} {user.last_name}'
+            request.data._mutable = _mutable
         if request.data['status'] == 'Clôturée' and user.is_super_admin == False:
             return Response({"Error": "you don't have permission"}, status=status.HTTP_401_UNAUTHORIZED)
         serializer.is_valid(raise_exception=True)
