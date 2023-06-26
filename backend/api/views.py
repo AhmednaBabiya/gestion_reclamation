122dd2eb3a5f9a94f9b4828115da2532c8d91a41
AhmednaBabiya <babiyaahmedna@gmail.com>
2023-06-26 10:47:20 +0000

    enhanced export view

--- backend/api/views.py
+++ backend/api/views.py
@@ -18,6 +18,59 @@ from backend.api.serializers import ProfileSerializer, ReclamationSerializer, Re
 from backend.api.pagination import ReclamationPagination, ProfilePagination
 
 
+# @api_view(['POST'])
+# def export_to_csv(request):
+#     begin_date = request.data['begin_date']
+#     end_date = request.data['end_date']
+#     reclamations = Reclamation.objects.filter(
+#         created_at__range=[begin_date, end_date]).order_by('created_at')
+#     response = HttpResponse(content_type='text/csv; charset=utf-8')
+#     date = datetime.datetime.now()
+#     date = date.strftime('%d-%m-%Y_%H:%M')
+#     response['Content-Disposition'] = f'attachment; filename=reclamation_export {date}.csv'
+#     # Use the codecs module to open the response object in write mode with UTF-8 encoding
+#     csv_file = codecs.getwriter('utf-8')(response)
+#     writer = csv.writer(csv_file)
+#     writer.writerow(['Nom complet', 'Telephone', 'NNI', "créer par", 'date de creation', 'Traitee par', 'date de traitement',
+#                     'type', 'statut', "lien carte d'identite", 'lien photo', "lien capture d'ecran", "commentaire", "date du commentaire"])
+#     reclamation_fields = reclamations.values_list(
+#         'customer_name', 'customer_phone_number', 'customer_nni_number', 'created_by', 'created_at', 'updated_by', 'treatment_date',
+#         'type', 'status', 'identity_card', 'photo', 'screenshot', 'commentary', 'error_date')
+#     for reclamation in reclamation_fields:
+#         created_at_formatted = reclamation[4].strftime('%d-%m-%Y %H:%M:%S')
+#         treatment_date = reclamation[6]
+#         error_date = reclamation[13]
+#         if treatment_date is not None:
+#             print("Treatment date exists:", treatment_date)
+#             treatment_date_formatted = treatment_date.strftime(
+#                 '%d-%m-%Y %H:%M:%S')
+#             print("Treatment date formatted:", treatment_date_formatted)
+#         if error_date is not None:
+#             error_date_formatted = error_date.strftime(
+#                 '%d-%m-%Y %H:%M:%S')
+#         else:
+#             print("Treatment date is None")
+#             treatment_date_formatted = ''
+#             error_date_formatted = ''
+#         modified_reclamation = [
+#             reclamation[0],
+#             reclamation[1],
+#             reclamation[2],
+#             reclamation[3],
+#             created_at_formatted,
+#             reclamation[5],
+#             treatment_date_formatted,
+#             reclamation[7],
+#             reclamation[8],
+#             reclamation[9],
+#             reclamation[10],
+#             reclamation[11],
+#             reclamation[12],
+#             error_date_formatted
+#         ]
+#         writer.writerow(modified_reclamation)
+#     return response
+
 @api_view(['POST'])
 def export_to_csv(request):
     begin_date = request.data['begin_date']
@@ -25,50 +78,38 @@ def export_to_csv(request):
     reclamations = Reclamation.objects.filter(
         created_at__range=[begin_date, end_date]).order_by('created_at')
     response = HttpResponse(content_type='text/csv; charset=utf-8')
-    date = datetime.datetime.now()
-    date = date.strftime('%d-%m-%Y_%H:%M')
+    date = datetime.datetime.now().strftime('%d-%m-%Y_%H:%M')
     response['Content-Disposition'] = f'attachment; filename=reclamation_export {date}.csv'
+
     # Use the codecs module to open the response object in write mode with UTF-8 encoding
     csv_file = codecs.getwriter('utf-8')(response)
     writer = csv.writer(csv_file)
     writer.writerow(['Nom complet', 'Telephone', 'NNI', "créer par", 'date de creation', 'Traitee par', 'date de traitement',
-                    'type', 'statut', "lien carte d'identite", 'lien photo', "lien capture d'ecran", "commentaire", "date du commentaire"])
-    reclamation_fields = reclamations.values_list(
-        'customer_name', 'customer_phone_number', 'customer_nni_number', 'created_by', 'created_at', 'updated_by', 'treatment_date',
-        'type', 'status', 'identity_card', 'photo', 'screenshot', 'commentary', 'error_date')
-    for reclamation in reclamation_fields:
-        created_at_formatted = reclamation[4].strftime('%d-%m-%Y %H:%M:%S')
-        treatment_date = reclamation[6]
-        error_date = reclamation[13]
-        if treatment_date is not None:
-            print("Treatment date exists:", treatment_date)
-            treatment_date_formatted = treatment_date.strftime(
-                '%d-%m-%Y %H:%M:%S')
-            print("Treatment date formatted:", treatment_date_formatted)
-        if error_date is not None:
-            error_date_formatted = error_date.strftime(
-                '%d-%m-%Y %H:%M:%S')
-        else:
-            print("Treatment date is None")
-            treatment_date_formatted = ''
-            error_date_formatted = ''
+                     'type', 'statut', "lien carte d'identite", 'lien photo', "lien capture d'ecran", "commentaire", "date du commentaire"])
+
+    for reclamation in reclamations:
+        created_at_formatted = reclamation.created_at.strftime('%d-%m-%Y %H:%M:%S')
+        treatment_date_formatted = reclamation.treatment_date.strftime('%d-%m-%Y %H:%M:%S') if reclamation.treatment_date else ''
+        error_date_formatted = reclamation.error_date.strftime('%d-%m-%Y %H:%M:%S') if reclamation.error_date else ''
+        
         modified_reclamation = [
-            reclamation[0],
-            reclamation[1],
-            reclamation[2],
-            reclamation[3],
+            reclamation.customer_name,
+            reclamation.customer_phone_number,
+            reclamation.customer_nni_number,
+            reclamation.created_by,
             created_at_formatted,
-            reclamation[5],
+            reclamation.updated_by,
             treatment_date_formatted,
-            reclamation[7],
-            reclamation[8],
-            reclamation[9],
-            reclamation[10],
-            reclamation[11],
-            reclamation[12],
+            reclamation.type,
+            reclamation.status,
+            reclamation.identity_card.url if reclamation.identity_card else '',
+            reclamation.photo.url if reclamation.photo else '',
+            reclamation.screenshot.url if reclamation.screenshot else '',
+            reclamation.commentary,
             error_date_formatted
         ]
         writer.writerow(modified_reclamation)
+
     return response
 
 
